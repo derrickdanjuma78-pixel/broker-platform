39252c04c4a778c539035600a50aa9a94ea3d592
derrickdanjuma78-pixel <derrickdanjuma78@gmail.com>
2026-05-10 13:23:47 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-pages/index.js
+export default function Home() {
+  return (
+    <div style={{ padding: 20, fontFamily: "Arial" }}>
+      <h1>Broker Platform</h1>
+      <p>Crypto Trading Simulator</p>
+      <a href="/trade">Go to Trade</a>
+    </div>
+  );
+}
