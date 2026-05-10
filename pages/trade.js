e8c5f90ccddb86f181ef22b97a61244823d79db4
derrickdanjuma78-pixel <derrickdanjuma78@gmail.com>
2026-05-10 13:24:07 +0100

    Update trade.js

--- pages/trade.js
+++ pages/trade.js
@@ -1,1 +1,9 @@
-pages/trade.js
+export default function Trade() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Trade Page</h1>
+      <p>Build working version next</p>
+      <a href="/">Back Home</a>
+    </div>
+  );
+}
